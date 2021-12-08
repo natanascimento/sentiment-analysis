6717a301144abde3a107f0b4e2a6c3702e8c69a4
Natan Nascimento <natan.nascimento@xpi.com.br>
2021-12-07 21:05:38 -0300

    feat: added main to run streamlit pages

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
-from models import ModelClassifier
+import streamlit as st
+from core.multipage import MultiPage
+from pages import sentiment_analysis_with_vader, sentiment_analysis_with_lstm
 
-if __name__ == '__main__':
+app = MultiPage()
 
-    ModelClassifier().execute()
+st.title("Sentiment Analysis - Machine Learning Class")
+
+app.add_page("Sentiment Analysis with VADER", sentiment_analysis_with_vader.app)
+app.add_page("Sentiment Analysis with LSTM", sentiment_analysis_with_lstm.app)
+
+app.run()
